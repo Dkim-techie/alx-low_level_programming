843930ea890ae0cdffd0f6f6acc4e64a14256433
Dkim-techie <daviswiner@gmail.com>
2023-02-16 16:21:08 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #!/bin/bash
-#include <stdio.h>
-
-int main(void)
-{
-    put("Programming is like building a multilingual puzzle");
-
-    return 0;
+#include<stdio.h>
+int main() {
+   puts("Programming is like building a multilingual puzzle");
+ 
+   return 0;
 }

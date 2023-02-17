a2f2c8f3ce71701745e435ba9b02b8cfe84536e9
Dkim-techie <daviswiner@gmail.com>
2023-02-16 17:08:04 -0800

    100-intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - Entry
- * Description: You are not allowed to use any functions listed in the NAME section of the man (3) printf or man (3) puts
-
- * Return: 1
+ * main - Entry point
+ * Description: Uses inline assembly to prnt string usng syscall func' (x86_64)
+ * Return: Always 1 (Success)
  */
 
 int main(void)
-
-	{
+{
 	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	long l = 59;
 	long fd = 1;
 	long syscall = 1;
 	long ret = 0;
+
+	__asm__ ("syscall"
+		: "=a" (ret)
+		: "a" (syscall),
+		"D" (fd),
+		"S" (s),
+		"d" (l));
 	return (1);
-}

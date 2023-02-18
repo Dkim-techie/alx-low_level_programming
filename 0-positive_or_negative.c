ddd97ae13d784e8f46dc2cd08af25f1ef5a295e1
Dkim-techie <daviswiner@gmail.com>
2023-02-18 00:42:32 -0800

    0-positive_or_negative.c

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,18 @@
+ #include <stdio.h>
+ /**
+  *
+  * main ( hello world);
+  * return (o);
+*/
+
+int main(void)
+    int = n;
+{
+if n>0
+	printf("is positive");
+if n=0
+	printf("is zero");
+else
+	printf("is negative");
+return (0);
+}

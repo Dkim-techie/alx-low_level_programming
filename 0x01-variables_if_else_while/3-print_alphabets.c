8631235f3f2fc963c1e3bc6ceb5bc5f9ce9758ec
Dkim-techie <daviswiner@gmail.com>
2023-02-20 13:19:48 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,26 +2,23 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there 
- * Main -a program that prints the alphabet in lowercase, followed by a new line.
- * Description You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
+ * Main -a program that prints lowercase.
+ * Description the putchar function
  * return(0) 
- *  betty style doc for function main goes there
  */
 
 int main(void)
 
-	{
-	int ch;
-
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-		putchar(ch);
-	}
-	for (ch = 'A'; ch <= 'Z'; ch++)
-	{
-		putchar(ch);
-	}
-	putchar('\n');
-
-	return (0);
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+for (ch = 'A'; ch <= 'Z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
 	}

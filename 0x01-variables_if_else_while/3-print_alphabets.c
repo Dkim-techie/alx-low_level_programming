11fb1effccbd4095d50195da2adeb6a0205e20b1
Dkim-techie <daviswiner@gmail.com>
2023-02-19 20:01:55 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,13 +9,19 @@
  */
 
 int main(void)
-{
+
+	{
 	int ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
 	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
 	putchar('\n');
+
 	return (0);
-}
+	}

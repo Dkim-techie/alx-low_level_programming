9d7b9e2018a5de066f87905a880f57de7012ea67
Dkim-techie <daviswiner@gmail.com>
2023-02-22 13:39:44 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/*
+ * main -prints the alphabet, in lowercase
+ * return (void)
+ */
+
+void print_alphabet(void)
+
+{
+	char c;
+       for (c = 'a'; c<= 'z'; c++)
+       {
+	       _putchar (c);
+       }
+
+       _putchar('\n');
+
+ 
+}
+

b5eea184b1e250b61544a6a0a9a362ea6a0c6878
Dkim-techie <daviswiner@gmail.com>
2023-02-22 13:51:15 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,10 +3,10 @@
 /*
  * main -prints the alphabet, in lowercase
  * return (void)
+ * Description for function print_alphabet
  */
 
 void print_alphabet(void)
-
 {
 char c;
 for (c = 'a'; c <= 'z'; c++)
